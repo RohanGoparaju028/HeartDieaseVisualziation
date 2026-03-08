95f6b0e3f0e38818ac84ce6e502ef8b782bfe3fa
Rohan Goparaju <goparajurohan41@gmail.com>
2026-03-08 16:42:42 -0400

    reated Tensor for features and label

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
 const d3 = require('d3');
+const tf = require('@tensorflow/tfjs');
 function GetData(file) {
     const csv = fs.readFileSync(file,'utf-8');
     const data = d3.csvParse(csv);
-
+    const exclude = ["id","Heart Diease"];
+    let features = data.map(row => {
+        const feature = {};
+        Object.keys(row).forEach(key => {
+            if(!exclude.includes(key)) {
+                feature[key] = +row[key];
+            }
+        })
+        return Object.values(feature);
+    });
+    let label = data.map(d => d["Heart Diease"] == "Presence" ? 1 : 0);
+    let X = tf.tensor2d(features);
+    let y = tf.tensor1d(label);
+    console.log(X.shape);
+    console.log(y.shape);
+    return {X,y};
 }
 function main(){
     const file = "HeartDieaseData.csv"
@@ -11,6 +27,8 @@ function main(){
         console.log("csv file  does not exist in the current directory");
         process.exit(1);
     }
-    GetData(file);
+    let {X,y} =  GetData(file);
+    console.log(X);
+    console.log(y);
 }
 main()
